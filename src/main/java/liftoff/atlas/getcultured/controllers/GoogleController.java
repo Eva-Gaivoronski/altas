fe3d1470de042cd9560c27fbe1b1b0838817df1d
Cresence <devharris93@gmail.com>
2023-12-14 15:42:04 -0500

    getCityStops method created for template of external API call to receive necessary JSON of user defined results from Places API. Needs long+lat of CityGeocode that has yet to be utilized by method at this time.

--- src/main/java/liftoff/atlas/getcultured/controllers/GoogleController.java
+++ src/main/java/liftoff/atlas/getcultured/controllers/GoogleController.java
@@ -2,6 +2,7 @@ package liftoff.atlas.getcultured.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
 @Controller
@@ -11,10 +12,25 @@ public class GoogleController {
     @PostMapping("results")
     private static void getCityGeocode(String searchTerm)
     {
-        final String apiKey = "&key=ENV_APIKEY_HERE";
+        final String apiKey = "&key=" + "ENV_APIKEY_HERE";
         final String uriStart = "https://maps.googleapis.com/maps/api/geocode/json?address=";
         final String uriEnd = "&region=us";
         final String uri = uriStart + searchTerm + uriEnd + apiKey;
+
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(uri, String.class);
+
+        System.out.println(result);
+    }
+
+//    getCityStops takes in the searchType (Restaurant, Bar, Venue, Museum, etc.) and the longitude and latitude (getCityGeocode()) with a hard-coded radius that produces a JSON of places.
+    @PostMapping("stops")
+    private static void getCityStops(@RequestParam String searchType, @RequestParam(required = true) String lnglat) {
+
+        final String apiKey = "&key=" + "ENV_APIKEY_HERE";
+        final String uriStart = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=";
+        final String uriEnd = "&" + lnglat;
+        final String uri = uriStart + searchType + uriEnd + "&radius=250" + apiKey;
 
         RestTemplate restTemplate = new RestTemplate();
         String result = restTemplate.getForObject(uri, String.class);
