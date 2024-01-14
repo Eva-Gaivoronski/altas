2fa8157ac76380a620eed03b92e67227e332dbf6
Alex Merchant <alex.m.merchant92@gmail.com>
2024-01-13 23:52:39 -0600

    Added basic user authorization
    * Added whitelisting to the AuthenticationFilter
    * Added a configuration file to add the authentication filter to the
        handler interceptor

--- src/main/java/liftoff/atlas/getcultured/AuthenticationFilter.java
+++ src/main/java/liftoff/atlas/getcultured/AuthenticationFilter.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 public class AuthenticationFilter implements HandlerInterceptor {
 
@@ -19,8 +21,30 @@ public class AuthenticationFilter implements HandlerInterceptor {
     @Autowired
     AuthenticationController authenticationController;
 
+    private static final List<String> whitelist = Arrays.asList(
+            "/",
+            "/user/sign-up",
+            "/user/login",
+            "/script.js",
+            "/styles.css",
+            "/user-styles.css"
+    );
+
+    private static boolean isWhitelisted(String path) {
+        for (String pathRoot : whitelist) {
+            if (path.equals(pathRoot)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
+        if(isWhitelisted((request.getRequestURI()))) {
+            return true;
+        }
+
         HttpSession session = request.getSession();
         User user = authenticationController.getUserFromSession(session);
 
